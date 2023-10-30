57de9eaed026ab647de54916bdef14e3b4c498a7
dizzysch <victorwilbert@gmail.com>
2023-10-30 12:33:23 -0300

    feat: add files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Blog.Data;
+using Blog.Models;
+
+using (var ctx = new BlogDataContext())
+{
+  var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+  ctx.Tags.Add(tag);
+  ctx.SaveChanges();
+}
