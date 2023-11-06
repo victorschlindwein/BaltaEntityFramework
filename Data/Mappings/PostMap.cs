463bb45787d91dfd46e2d0f69e032d96516eb693
dizzysch <victorwilbert@gmail.com>
2023-11-06 11:51:24 -0300

    feat: testando mapeamentos

--- Data/Mappings/PostMap.cs
+++ Data/Mappings/PostMap.cs
@@ -12,18 +12,18 @@ namespace Blog.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<Post> builder)
         {
-            // Tabela
+            //Tabela
             builder.ToTable("Post");
 
-            // Chave Primária
+            //Chave Primária
             builder.HasKey(x => x.Id);
 
-            // Identity
+            //Identity
             builder.Property(x => x.Id)
                 .ValueGeneratedOnAdd()
                 .UseIdentityColumn();
 
-            // Propriedades
+            //Propriedades
             builder.Property(x => x.LastUpdateDate)
                 .IsRequired()
                 .HasColumnName("LastUpdateDate")
@@ -32,10 +32,39 @@ namespace Blog.Data.Mappings
                 .HasDefaultValueSql("GETDATE()");
             // .HasDefaultValue(DateTime.Now.ToUniversalTime());
 
-            // Índices
+            //Índices
             builder
                 .HasIndex(x => x.Slug, "IX_Post_Slug")
                 .IsUnique();
+
+            //Relacionamentos
+            builder.HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Category)
+               .WithMany(x => x.Posts)
+               .HasConstraintName("FK_Post_Category")
+               .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post
+                        .HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag
+                        .HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new BlogDataContext();
+
+// context.Users.Add(new User
+// {
+//   Bio = "Vitao",
+//   Email = "eu@victorschlindwein.com",
+//   Image = "vitao.jpeg",
+//   Name = "Vitao",
+//   PasswordHash = "vitao123",
+//   Slug = "vitao",
+// }
+// );
+// context.SaveChanges();
+
+var user = context.Users.FirstOrDefault(x => x.Name.Equals("Vitao"));
+var category = context.Categories.FirstOrDefault(x => x.Name.Equals("backend"));
+
+var post = new Post
+{
+  Author = user,
+  Body = "Corpo do artigo",
+  Category = category,
+  CreateDate = System.DateTime.Now,
+  // LastUpdateDate =
+  Slug = "artigo-do-vitao",
+  Summary = "Neste artigo vamos falar...",
+  //Tags = null,
+  Title = "Titulo do artigo"
+};
+
+context.Add(post);
+context.SaveChanges();
