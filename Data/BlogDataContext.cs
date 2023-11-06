bcb3059bbfc9a8e49fd44d0f2611dc16e5350f71
dizzysch <victorwilbert@gmail.com>
2023-11-06 10:07:49 -0300

    feat: limpando data context para fluent mapping

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -9,12 +9,6 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-        public DbSet<User> Users { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
