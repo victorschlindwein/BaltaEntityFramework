b56c5fe65af605f8ac6df39e970415e09aa7ca63
dizzysch <victorwilbert@gmail.com>
2023-11-06 17:17:39 -0300

    feat: implementa relacionamentos e migration

--- Data/Mappings/UserMap.cs
+++ Data/Mappings/UserMap.cs
@@ -32,6 +32,7 @@ namespace Blog.Data.Mappings
             builder.Property(x => x.Email);
             builder.Property(x => x.Image);
             builder.Property(x => x.PasswordHash);
+            builder.Property(x => x.GitHub);
 
             builder.Property(x => x.Slug)
                 .IsRequired()
--- Program.cs
+++ Program.cs
@@ -4,32 +4,33 @@ using Microsoft.EntityFrameworkCore;
 
 using var context = new BlogDataContext();
 
-// context.Users.Add(new User
-// {
-//   Bio = "Vitao",
-//   Email = "eu@victorschlindwein.com",
-//   Image = "vitao.jpeg",
-//   Name = "Vitao",
-//   PasswordHash = "vitao123",
-//   Slug = "vitao",
-// }
-// );
-// context.SaveChanges();
+context.Users.Add(new User
+{
+    Bio = "Vitao",
+    Email = "eu@victorschlindwein.com",
+    Image = "vitao.jpeg",
+    Name = "Vitao",
+    PasswordHash = "vitao123",
+    GitHub = "github.com/victorschlindwein",
+    Slug = "vitao",
+}
+);
+context.SaveChanges();
 
 var user = context.Users.FirstOrDefault(x => x.Name.Equals("Vitao"));
 var category = context.Categories.FirstOrDefault(x => x.Name.Equals("backend"));
 
 var post = new Post
 {
-  Author = user,
-  Body = "Corpo do artigo",
-  Category = category,
-  CreateDate = System.DateTime.Now,
-  // LastUpdateDate =
-  Slug = "artigo-do-vitao",
-  Summary = "Neste artigo vamos falar...",
-  //Tags = null,
-  Title = "Titulo do artigo"
+    Author = user,
+    Body = "Corpo do artigo",
+    Category = category,
+    CreateDate = System.DateTime.Now,
+    // LastUpdateDate =
+    Slug = "artigo-do-vitao",
+    Summary = "Neste artigo vamos falar...",
+    //Tags = null,
+    Title = "Titulo do artigo"
 };
 
 context.Add(post);
